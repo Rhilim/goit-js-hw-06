a7ff730aa493efdaa27ea41d176338e1cc2a6f0e
Rhilim <arhilim@gmail.com>
2023-05-31 05:29:11 -0500

    resolved task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,26 +7,36 @@ const ingredients = [
   'Condiments',
 ];
 
+const listMain = document.querySelector('#ingredients');
+
+const listEl1 = document.createElement('li');
+listEl1.textContent = ingredients[0];
+listEl1.classList.add('item');
+
+const listEl2 = document.createElement('li');
+listEl2.textContent = ingredients[1];
+listEl2.classList.add('item');
+
+const listEl3 = document.createElement('li');
+listEl3.textContent = ingredients[2];
+listEl3.classList.add('item');
+
+const listEl4 = document.createElement('li');
+listEl4.textContent = ingredients[3];
+listEl4.classList.add('item');
+
+const listEl5 = document.createElement('li');
+listEl5.textContent = ingredients[4];
+listEl5.classList.add('item');
+
+const listEl6 = document.createElement('li');
+listEl6.textContent = ingredients[5];
+listEl6.classList.add('item');
 
 
+listMain.append(listEl1, listEl2, listEl3, listEl4, listEl5, listEl6);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.dir('#ingredients');
 
 // HTML містить порожній список ul#ingredients.
 
