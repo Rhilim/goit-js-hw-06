d6c8009f9cec1d9bef3d31d6d6aa346163a1a195
Rhilim <arhilim@gmail.com>
2023-06-02 05:12:18 -0500

    task 4 resolved

--- js/task-04.js
+++ js/task-04.js
@@ -11,3 +11,38 @@
 //     ініціалізуй її значенням 0.
 //     Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 //     Оновлюй інтерфейс новим значенням змінної counterValue.
+
+
+
+const counterValue = {
+  value: 0,
+  increment() {
+    console.log("increment -> this", this);
+    this.value += 1;
+  },
+  decrement() {
+    console.log("decrement -> this", this);
+    this.value -= 1;
+  },
+};
+
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const valueEl = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', function () {
+    console.log('Клікнули на кнопку Декремент');
+
+    counterValue.decrement();
+    console.log(counterValue);
+
+valueEl.textContent = counterValue.value;
+});
+incrementBtn.addEventListener("click", function () {
+  console.log("Клікнули на кнопку Інкремент");
+  
+  counterValue.increment();
+  console.log(counterValue);
+
+  valueEl.textContent = counterValue.value;
+});
